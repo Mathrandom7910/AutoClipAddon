bbb4f6345e34599dd08227d69b4e6d59d4dcddd6
0xEnjoy <104784056+0xEnjoy@users.noreply.github.com>
2022-05-06 15:57:51 +0300

    Add files via upload

---
+++ src/main/java/me/mathrandom7910/AutoClip/commands/AVClipCommandFix.java
@@ -0,0 +1,46 @@
+package me.mathrandom7910.AutoClip.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import meteordevelopment.meteorclient.systems.commands.Command;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.command.CommandSource;
+import net.minecraft.util.math.BlockPos;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+
+public class AVClipCommandFix extends Command {
+    private MinecraftClient mc = MinecraftClient.getInstance();
+    public AVClipCommandFix() {
+        super("avcilpfixed", "the best idea", "avcf");
+    }
+
+    private Block getBlock(BlockPos pos) {
+        return mc.player.world.getBlockState(pos).getBlock();
+    }
+
+    private boolean doAutoClip(int incr) {
+        BlockPos pos = mc.player.getBlockPos();
+        //      -1            -10          -1
+        if(incr == 0) incr = 1;
+        for(int i = incr; incr > 0 ? i <= 10 : i >= -10; i += incr) {
+            if(getBlock(pos.add(0, i, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 1, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 2, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 3, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 4, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 5, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 6, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 7, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 8, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 9, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 10, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 11, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 12, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 13, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 14, 0)) == Blocks.AIR) {
+                ChatUtils.info("Found clip block " + i + " blocks " + (incr > 0 ? "up" : "down") + ".");
+                mc.player.setPosition(pos.getX(), pos.getY() + i, pos.getZ());
+                return true;
+            }
+        }
+        ChatUtils.error("Unable to clip.");
+        return false;
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.then(literal("up").executes(c -> {
+            doAutoClip(1);
+            return SINGLE_SUCCESS;
+        }));
+    }
+}
