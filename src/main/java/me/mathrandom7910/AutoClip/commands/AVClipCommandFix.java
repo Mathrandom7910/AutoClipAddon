0565ac16872c22460caf031b386c65d3c2804e9b
0xEnjoy <104784056+0xEnjoy@users.noreply.github.com>
2022-05-06 19:55:09 +0300

    Update AVClipCommandFix.java

--- src/main/java/me/mathrandom7910/AutoClip/commands/AVClipCommandFix.java
+++ src/main/java/me/mathrandom7910/AutoClip/commands/AVClipCommandFix.java
@@ -9,13 +9,14 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.CommandSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class AVClipCommandFix extends Command {
     private MinecraftClient mc = MinecraftClient.getInstance();
     public AVClipCommandFix() {
-        super("avcilpfixed", "the best idea", "avcf");
+        super("avcilp", "a terrible idea", "avc");
     }
 
     private Block getBlock(BlockPos pos) {
@@ -29,8 +30,11 @@ public class AVClipCommandFix extends Command {
         assert player != null;
         if(incr == 0) incr = 1;
         for(float i = incr; incr > 0 ? i <= 10 : i >= -10; i += incr) {
-            if(getBlock(pos.add(0, i, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 1, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 2, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 3, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 4, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 5, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 6, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 7, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 8, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 9, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 10, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 11, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 12, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 13, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 14, 0)) == Blocks.AIR) {
-                ChatUtils.info("Found clip block " + i + " blocks " + (incr > 0 ? "up" : "down") + ".");
+            if(getBlock(pos.add(0, i, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 1, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 2, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 3, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 4, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 5, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 6, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 7, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 8, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 9, 0)) == Blocks.AIR && getBlock(pos.add(0, i + 10, 0)) == Blocks.AIR) {                ChatUtils.info("Found clip block " + i + " blocks " + (incr > 0 ? "up" : "down") + ".");
+                            if (player.hasVehicle()) {
+                Entity vehicle = player.getVehicle();
+                vehicle.setPosition(vehicle.getX(), vehicle.getY() + i, vehicle.getZ());
+            }
                 player.setPosition(player.getX(), player.getY() + i, player.getZ());
                 return true;
             }
